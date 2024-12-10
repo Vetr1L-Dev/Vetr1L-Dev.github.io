792edb8acfe5684b07c206fb7e85a9d1a96e33e7
Vetr1L-Dev <brovetril@gmail.com>
2024-12-10 13:06:56 +0300

    1

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ tg.expand();
 
 document.getElementById("checkbox").addEventListener("change", () => {
     if (document.getElementById("checkbox").checked) {
-        document.getElementById("endbutton").style.display = "block";
+        document.getElementById("closeworkbutton").style.display = "block";
     } else {
-        document.getElementById("endbutton").style.display = "none";
+        document.getElementById("closeworkbutton").style.display = "none";
         document.getElementById("error").style.display = "none";
     }
 })
@@ -19,7 +19,13 @@ order.addEventListener("click", () => {
 document.getElementById('expenseschoice').addEventListener('change', function() {
     if (document.getElementById('expenseschoice').value == 'Деньги') {
         document.getElementById("salary").style.display = "block";
-        
+        document.getElementById("paragcheck").style.display = "block";
+    } else if (document.getElementById('expenseschoice').value == 'Не выбрано') {
+        document.getElementById("salary").style.display = "none";
+        document.getElementById("paragcheck").style.display = "none";
+    } else {
+        document.getElementById("salary").style.display = "none";
+        document.getElementById("paragcheck").style.display = "block";
     }
 
 })
