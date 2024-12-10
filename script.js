a7cd85e00dffb2809a030151ed046aca4c7a16a6
Vetr1L-Dev <brovetril@gmail.com>
2024-12-10 14:23:37 +0300

    1

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
 document.getElementById('choice').addEventListener('change', function(){
-    if (document.getElementById('choice').value == 'Не выбрано'){
-        document.getElementById('check').style.display == 'none'
-    } else {
-        document.getElementById('check').style.display == 'block'
-    }
+    document.getElementById('check').style.display == 'block'
 })
